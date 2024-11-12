5e29febd40bb5142a719c182d2dd88f030a84fa5
Bisal-Shafiq <93635077+Bisal-Shafiq@users.noreply.github.com>
2024-11-12 15:18:06 +0500

    Update About.tsx

--- app/compoent/About.tsx
+++ app/compoent/About.tsx
@@ -15,7 +15,7 @@ export default function About() {
       <div className="aboutContent">
         <div className="imageContainer">
           {/* Using the Next.js Image component */}
-          <img src="/img.jpg" alt="Bisal Shafiq" className="aboutImage" />
+          <img src="/img.jpg" alt="img" className="aboutImage" />
         </div>
         <div className="textContainer">
           <h1>Hi, I am Bisal Shafiq</h1>
